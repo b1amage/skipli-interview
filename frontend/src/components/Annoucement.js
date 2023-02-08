f151f88bcf5d820923a6d38f490ae8077306af5a
Nguyen Luu Quoc Bao <s3877698@rmit.edu.vn>
2023-02-08 09:35:17 +0700

    fix bug

--- frontend/src/components/Annoucement.js
+++ frontend/src/components/Annoucement.js
@@ -3,13 +3,11 @@ import React from "react";
 const Annoucement = ({ children, isError }) => {
   return (
     <div
-      className={`w-full px-5 py-3 ${isError ? "bg-red-300" : "bg-green-300"}`}
+      className={`rounded-lg w-full px-5 py-3 ${
+        isError ? "bg-red-300" : "bg-green-300"
+      }`}
     >
-      <h1
-        className={`${
-          isError ? "text-red-600" : "text-green-600"
-        } text-xl rounded-lg`}
-      >
+      <h1 className={`${isError ? "text-red-600" : "text-green-600"} text-xl`}>
         {children}
       </h1>
     </div>
